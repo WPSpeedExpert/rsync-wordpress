b47c611f02ec44f3eb4acaa1030ab3bd29f9c84b
WP Speed Expert <139096428+WPSpeedExpert@users.noreply.github.com>
2024-05-03 22:14:23 +0200

    Create rsync-pull-production-to-staging.sh

---
+++ rsync-pull-production-to-staging.sh
@@ -0,0 +1,164 @@
+#!/bin/bash
+# =========================================================================== #
+# Description:        Rsync the production website to the staging server/website
+# Details:            Rsync Pull to simplify the script.
+# Made for:           Linux, Cloudpanel (Debian & Ubuntu).
+# Requirements:       ssh-keygen - ssh-copy-id root@127.0.0.1 (replace IP)
+# Author:             WP Speed Expert
+# Author URI:         https://wpspeedexpert.com
+# Version:            0.5
+# Make executable:    chmod +x /home/staging/rsync-pull-production-to-staging.sh
+# Execute the script: sudo /home/staging/rsync-pull-production-to-staging.sh > /home/staging/rsync-pull-production-to-staging.log
+# Crontab @daily 2am: 0 2 * * * /home/staging/rsync-pull-production-to-staging.sh > /home/staging/rsync-pull-production-to-staging.log 2>&1
+# @weekly on sunday:  0 2 * * 7 /home/staging/rsync-pull-production-to-staging.sh > /home/staging/rsync-pull-production-to-staging.log 2>&1
+# =========================================================================== #
+#
+# Variables - Modify to match your situation
+PRODUCTION_URL=("production.domain.com")
+STAGING_URL=("staging.domain.com")
+#
+PRODUCTION_DATABASE=("production-database")
+STAGING_DATABASE=("staging-database")
+TABLE_PREFIX=("wp_")
+#
+PRODUCTION_SERVER_SSH=("root@0.0.0.0")
+#
+PRODUCTION_PATH=("/home/website/htdocs/production.domain.com")
+STAGING_PATH=("/home/staging/htdocs/staging.domain.com")
+#
+STAGING_SCRIPT_PATH=("/home/staging")
+PRODUCTION_SCRIPT_PATH=("/home/production")
+#
+STAGING_USER_GROUP=("user-staging")
+#
+CURRENT_DATE_TIME=$(date +"%Y-%m-%d %T")
+#
+# Log the date and time
+echo "[+] NOTICE: Start script: ${CURRENT_DATE_TIME}"
+
+# Check for WP directory & wp-config.php
+if [ ! -d ${STAGING_PATH} ]; then
+  echo "[+] ERROR: Directory ${STAGING_PATH} does not exist"
+  echo ""
+  exit
+fi
+if [ ! -f ${STAGING_PATH}/wp-config.php ]; then
+  echo "[+] ERROR: No wp-config.php in ${STAGING_PATH}"
+  echo ""
+fi
+  echo "[+] SUCCESS: Found wp-config.php in ${STAGING_PATH}"
+
+# Make a backup of the wp-config.php file
+echo "[+] NOTICE: Backup wp-content to: ${STAGING_SCRIPT_PATH}/tmp"
+cp "${STAGING_PATH}/wp-config.php" "${STAGING_SCRIPT_PATH}/tmp"
+#
+if [ ! -f ${STAGING_SCRIPT_PATH}/tmp/wp-config.php ]; then
+  echo "[+] ERROR: No wp-config.php in ${STAGING_SCRIPT_PATH}/tmp"
+  echo ""
+  exit
+fi
+  echo "[+] SUCCESS: Found wp-config.php in ${STAGING_SCRIPT_PATH}/tmp"
+
+# Stop nginx
+# echo "[+] NOTICE: Stop Nginx."
+# sudo systemctl stop nginx
+
+# Clean and remove website files (except for the uploads directory & exceptions)
+# find ${STAGING_PATH}/ -mindepth 1 ! -regex '^'${STAGING_PATH}'/wp-config.php' ! -regex '^'${STAGING_PATH}'/wp-content/uploads\(/.*\)?' -delete
+#
+# Clean and remove destination website files (except for the wp-config.php)
+find ${STAGING_PATH}/ -mindepth 1 ! -regex '^'${STAGING_PATH}'/wp-config.php' -delete
+#
+# Copy back the wp-config.php file
+# cp "${STAGING_SCRIPT_PATH}/tmp/wp-config.php" "${STAGING_PATH}"
+#
+if [ ! -f ${STAGING_PATH}/wp-config.php ]; then
+  echo "[+] ERROR: No wp-config.php in ${STAGING_PATH}"
+  echo ""
+  exit
+fi
+  echo "[+] SUCCESS: Found wp-config.php in ${STAGING_PATH}"
+
+# Export the remote MySQL database |  ${PRODUCTION_PATH}
+echo "[+] NOTICE: Export the remote database: ${PRODUCTION_DATABASE}"
+ssh ${PRODUCTION_SERVER_SSH} "clpctl db:export --databaseName=${PRODUCTION_DATABASE} --file=${PRODUCTION_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz"
+echo "[+] NOTICE: Synching the database: ${PRODUCTION_DATABASE}.sql.gz"
+rsync -azP ${PRODUCTION_SERVER_SSH}:${PRODUCTION_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz ${STAGING_SCRIPT_PATH}/tmp
+
+# Cleanup remote database export file
+echo "[+] NOTICE: Clean up the remote database export file: ${PRODUCTION_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz"
+ssh ${PRODUCTION_SERVER_SSH} "rm ${PRODUCTION_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz"
+
+# Drop all database tables (clean up)
+echo "[+] NOTICE: Drop all database tables ..."
+#
+# Create a variable with the command to list all tables
+tables=$(mysql --defaults-extra-file=${STAGING_SCRIPT_PATH}/my.cnf -Nse 'SHOW TABLES' ${STAGING_DATABASE})
+#
+# Loop through the tables and drop each one
+for table in $tables; do
+    echo "[+] NOTICE: Dropping $table from ${STAGING_DATABASE} ..."
+    mysql --defaults-extra-file=${STAGING_SCRIPT_PATH}/my.cnf  -e "DROP TABLE $table" ${STAGING_DATABASE}
+done
+    echo "[+] SUCCESS: All tables dropped from ${STAGING_DATABASE}."
+
+# Import the MySQL database:
+echo "[+] NOTICE: Import the MySQL database: ${STAGING_DATABASE} ..."
+clpctl db:import --databaseName=${STAGING_DATABASE} --file=${STAGING_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz
+#
+echo "[+] NOTICE: Clean up the database export file: ${STAGING_DATABASE} ..."
+rm ${STAGING_SCRIPT_PATH}/tmp/${PRODUCTION_DATABASE}.sql.gz
+
+# Search and replace URL in the database
+echo "[+] NOTICE: Search and replace URL in the database: ${STAGING_DATABASE} ..."
+mysql --defaults-extra-file=${STAGING_SCRIPT_PATH}/my.cnf -D ${STAGING_DATABASE} -e "
+UPDATE ${TABLE_PREFIX}options SET option_value = REPLACE (option_value, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}') WHERE option_name = 'home' OR option_name = 'siteurl';
+UPDATE ${TABLE_PREFIX}posts SET post_content = REPLACE (post_content, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}posts SET post_excerpt = REPLACE (post_excerpt, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}postmeta SET meta_value = REPLACE (meta_value, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}termmeta SET meta_value = REPLACE (meta_value, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}comments SET comment_content = REPLACE (comment_content, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}comments SET comment_author_url = REPLACE (comment_author_url, 'https://${PRODUCTION_URL}','https://${STAGING_URL}');
+UPDATE ${TABLE_PREFIX}posts SET guid = REPLACE (guid, 'https://${PRODUCTION_URL}', 'https://${STAGING_URL}') WHERE post_type = 'attachment';
+"
+
+# Enable: Discourage search engines from indexing this website
+echo "[+] NOTICE: Enable discourage search engines from indexing this website."
+mysql --defaults-extra-file=${STAGING_SCRIPT_PATH}/my.cnf -D ${STAGING_DATABASE} -e "
+UPDATE ${TABLE_PREFIX}options SET option_value = replace (option_value, '1', '0') WHERE option_name = 'blog_public';
+"
+
+# Check if query was_successful
+echo "[+] NOTICE: Check if query was_successful."
+query=$(mysql --defaults-extra-file=${STAGING_SCRIPT_PATH}/my.cnf -D ${STAGING_DATABASE} -se "SELECT option_value FROM ${TABLE_PREFIX}options WHERE option_name = 'siteurl';")
+echo "[+] SUCCESS: Siteurl = $query."
+
+# Rsync website files (pull)
+echo "[+] NOTICE: Start Rsync pull"
+rsync -azP --update --delete --no-perms --no-owner --no-group --no-times --exclude 'wp-content/cache/*' --exclude 'wp-content/backups-dup-pro/*' --exclude 'wp-config.php' ${PRODUCTION_SERVER_SSH}:${PRODUCTION_PATH}/ ${STAGING_PATH}
+
+# Set correct ownership
+echo "[+] NOTICE: Set correct ownership."
+chown -Rf ${STAGING_USER_GROUP}:${STAGING_USER_GROUP} ${STAGING_PATH}
+
+# Set correct file permissions for folders
+echo "[+] NOTICE: Set correct file permissions for folders."
+chmod 00755 -R ${STAGING_PATH}
+
+# Set correct file permissions for files
+echo "[+] NOTICE: Set correct file permissions for files."
+find ${STAGING_PATH}/ -type f -print0 | xargs -0 chmod 00644
+
+# Flush & restart Redis
+echo "[+] NOTICE: Flush, restart and clean up Redis."
+redis-cli FLUSHALL
+sudo systemctl restart redis-server
+
+# Start nginx
+# echo "[+] NOTICE: Start Nginx."
+# sudo systemctl stop nginx
+# sudo systemctl status nginx
+
+# End of the script
+echo "[+] NOTICE: End of script: ${CURRENT_DATE_TIME}"
+exit
